634dcaf6053610f0e40510f9d1dec08878acf90a
JAMChannel <pon244tt@gmail.com>
2021-11-29 02:50:42 +0900

    チャットルーム完成

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,14 +1,9 @@
 class ChatroomsController < ApplicationController
-
   before_action :require_login, only: %i[index show create]
   before_action :require_user_ids, only: %i[create]
+
   def index
     @chatrooms = current_user.chatrooms.includes(:users, messages: :user).page(params[:page]).order(created_at: :desc)
-  end
-
-  def show
-    @chatroom = current_user.chatrooms.includes(:users).find(params[:id])
-    # binding.pry
   end
 
   def create
@@ -16,8 +11,11 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.chatroom_for_users(users)
     @messages = @chatroom.messages.order(created_at: :desc).limit(100).reverse
     @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
-    binding.pry
     redirect_to chatroom_path(@chatroom)
+  end
+
+  def show
+    @chatroom = current_user.chatrooms.includes(:users).find(params[:id])
   end
 
   private
